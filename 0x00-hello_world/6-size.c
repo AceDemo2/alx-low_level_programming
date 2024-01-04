3370e009bdf0ecdfc6f4c14256d380f71932150d
AceDemo2 <lawal641@gmail.com>
2024-01-04 08:15:09 -0800

    printf2

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *ty[] = {"char", "int", "long int", "long long int", "float"};
+	int i = 0;
+
+	while (i < sizeof(ty) / sizeof(ty[0]))
+		{
+			printf("Size of a %s: %lu bytes(s)\n", ty[i], sizeof(ty[i]));
+			i++;
+		}
+	return (0);
+	
