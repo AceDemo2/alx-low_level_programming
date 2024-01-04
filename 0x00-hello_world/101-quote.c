9785a6f5e9f99cb18c66ad072d63e3c7d5b24ca4
AceDemo2 <lawal641@gmail.com>
2024-01-04 09:05:17 -0800

    standard error.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
  * main - write to stdout
  * Return: Always 0
  */
-int main (void)
+int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
