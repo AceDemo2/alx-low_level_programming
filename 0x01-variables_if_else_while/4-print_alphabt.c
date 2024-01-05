c80da2a45bdca58fd6fc53f7f529eaace8dca8e6
AceDemo2 <lawal641@gmail.com>
2024-01-04 16:38:35 -0800

    skip e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Use `putchar` to print all letters but the letter 'q' and 'e'.
+ * Return: 0
+ */
+int main(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		if (a == 'e' || a == 'q')
+			continue;
+		else
+			putchar(a);
+		a++;
+	}
+	putchar('\n');
+}
