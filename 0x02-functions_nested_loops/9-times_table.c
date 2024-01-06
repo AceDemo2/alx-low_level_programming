5a2391500da58f0aa699189ce15f1c38d64b1955
AceDemo2 <lawal641@gmail.com>
2024-01-06 13:25:56 -0800

    9timestable

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,25 +15,29 @@ void times_table(void)
 		while (j <= 9)
 		{
 			k = i * j;
+			if (k == 0)
+				_putchar(k + '0');
 			if (k >= 10)
 			{
-				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
 				if (j != 9)
 				{
 					_putchar(',');
 					_putchar(' ');
 				}
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+				
 			}
 			else
 			{
-				_putchar(k + '0');
 				if (j != 9)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 				}
+				_putchar(k + '0');
+				
 			}
 			if (j == 9)
 				_putchar('\n');
