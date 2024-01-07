943c9d117076a0cbf7ac2aeea027f04815f27f76
AceDemo2 <lawal641@gmail.com>
2024-01-07 07:39:41 -0800

    sum fibonacci to 98

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,15 +7,33 @@
  */
 int main(void)
 {
-	unsigned long int i = 1, j = 2, k, l = 0;
+	unsigned long int i = 1, j = 2, k, k1, k2, i1, j1, i2, j2, l = 0;
 
 	printf("%lu, %lu", i, j);
-	while (l < 96)
+	while (l < 86)
 	{
 		k = i + j;
 		printf(", %lu", k);
 		i = j;
 		j = k;
+		l++;
+	}
+	i1 = i / 1000000000;
+	i2 = i % 1000000000;
+	j1 = j / 1000000000;
+	j2 = j % 1000000000;
+	k1 = k / 1000000000;
+	k2 = k % 1000000000;
+	while (l < 96)
+	{
+		k1 = i1 + j1;
+		k2 = i2 + j2;
+		printf(", %lu", k1);
+		printf("%lu", k2);
+		i1 = j1;
+		i2 = j2;
+		j1 = k1;
+		j2 = k2;
 		l++;
 	}
 	printf("\n");
