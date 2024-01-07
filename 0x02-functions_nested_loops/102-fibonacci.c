ae2d9ee4ac19bdf4db925ab81ac6af5b214b9b99
AceDemo2 <lawal641@gmail.com>
2024-01-07 04:38:03 -0800

    sum multiple 3 0r 5

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description: Print the first 50 fibonacci numbers, starting with 1 and 2.
+ * Numbers must be coma and space separated.
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 1, j = 2, k, l = 3;
+
+	printf("%d, %d", a, b);
+	while(l <= 50)
+	{
+		k = i + j;
+		printf(", %d", k);
+		i = j;
+		j = k;
+		l++;
+	}
+}
