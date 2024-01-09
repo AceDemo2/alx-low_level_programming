edae8c28e1726070225bd7b473d1c34541951292
AceDemo2 <lawal641@gmail.com>
2024-01-09 15:17:25 -0800

    print number using _putchar

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,13 +6,24 @@
  */
 void print_number(int n)
 {
-	if (n < 0)
-	{
-		_putchar('-');
-		n = -n;
-	}
-	if (n / 10 != 0)
-		print_number(n / 10);
-	_putchar((n % 10) + '0');
+    if (n == INT_MIN)
+    {
+        // Handle the special case of INT_MIN
+        _putchar('-');
+        _putchar('2'); // Digit in the ten-millions place
+        n %= 1000000000; // Remove the ten-millions digit
+        n = -n; // Make the remaining part positive
+    }
+
+    if (n < 0)
+    {
+        _putchar('-');
+        n = -n;
+    }
+
+    if (n / 10 != 0)
+        print_number(n / 10);
+
+    _putchar((n % 10) + '0');
 }
 
