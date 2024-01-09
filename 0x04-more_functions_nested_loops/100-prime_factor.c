39306ff532594be6df78db31f37d387de10e69a3
AceDemo2 <lawal641@gmail.com>
2024-01-09 11:10:41 -0800

    argest iim number

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - main block
+ * Description: Find and print the largest prime factor of the number
+ * 612852475143
+ * Return: 0
+ */
+int main(void)
+{
+	long i, j = 612852475143;
+
+	for (i = 2; i <= j; i++)
+	{
+		while (j % i == 0)
+			j /= i;
+	}
+	putchar(j + '0')
+	return (0);
+}
