1b983d9a9adcc1c86a2f28ebc0fab109179d4b14
AceDemo2 <lawal641@gmail.com>
2024-01-09 15:34:13 -0800

    print number using _putchar

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,28 +1,20 @@
 #include "main.h"
-#include <limits.h>
+
 /**
  * print_number - Prints an integer
  * @n: Integer to be printed
  */
 void print_number(int n)
 {
-    if (n == INT_MIN)
-    {
-        _putchar('-');
-        _putchar('2');
-        n %= 1000000000;
-        n = -n;
-    }
+	unsigned int n1 = n;
 
-    if (n < 0)
-    {
-        _putchar('-');
-        n = -n;
-    }
-
-    if (n / 10 != 0)
-        print_number(n / 10);
-
-    _putchar((n % 10) + '0');
+	if (n1 < 0)
+	{
+		_putchar('-');
+		n1 = -n1;
+	}
+	if (n1 / 10 != 0)
+		print_number(n1 / 10);
+	_putchar((n1 % 10) + '0');
 }
 
