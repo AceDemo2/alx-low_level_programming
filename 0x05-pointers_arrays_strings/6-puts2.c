195cc866c45b06d0ae4c4ce9c394a13d612e2705
AceDemo2 <lawal641@gmail.com>
2024-01-10 08:25:15 -0800

    prints every other char

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,8 +10,10 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (str[i - 1] != '\0' || str[i] != '\0')
+	while (str[i] != '\0')
 	{
+		if (str[i - 1] == '\0')
+			break;
 		_putchar(str[i]);
 		i += 2;
 	}
