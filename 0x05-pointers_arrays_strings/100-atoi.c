bdc0f4c6b7c9c40d058a312b2f4a4a85992950af
AceDemo2 <lawal641@gmail.com>
2024-01-10 17:19:47 -0800

    convert to string

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _atoi - Convert a string to an integer.
+ * @s: The pointer to convert
+ *
+ * Return: A integer
+ */
+int _atoi(char *s)
+{
+	int i = 0, j = 0, k = 1;
+
+	while (s[i])
+	{
+		if (s[i] == '-')
+			k = -k;
+		if (s[i] <= '0' && s[i] >= '9')
+			continue;
+		else
+		j = j * 10 + (s[i] - '0');
+		i++;
+	}
+	return (j * k);
+}
