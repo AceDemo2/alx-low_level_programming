5b243bf169e3b466d5081d8e2feb437048083d69
AceDemo2 <lawal641@gmail.com>
2024-01-10 18:12:33 -0800

    convert to string

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,25 +8,25 @@
  */
 int _atoi(char *s)
 {
-    unsigned int i = 0, j = 0, k = 1;
+	unsigned int i = 0, j = 0, k = 1;
 
-    while (s[i])
-    {
-        if (s[i] == '-')
-        {
-            k = -k;
-        }
-        else if (s[i] >= '0' && s[i] <= '9')
-        {
-            j = j * 10 + (s[i] - '0');
-        }
-        else
-        {
-            if (j != 0)
-                break;
-        }
-        i++;
-    }
+	while (s[i])
+	{
+		if (s[i] == '-')
+		{
+			k = -k;
+		}
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			j = j * 10 + (s[i] - '0');
+		}
+		else if (j != 0)
+		{
+			break;
+		}
+		i++;
+	}
 
-    return (j * k);
+	return (j * k);
 }
+
