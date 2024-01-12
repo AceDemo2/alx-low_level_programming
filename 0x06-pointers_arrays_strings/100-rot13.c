b2765c226a9734c1d4bd34c2e2c5b4ebcb4368b8
AceDemo2 <lawal641@gmail.com>
2024-01-12 15:26:25 -0800

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,1 +1,25 @@
-sh
+#include "main.h"
+
+/**
+ * rot13 - encodes a string into rot13
+ * @s: string to encode
+ *
+ * Return: address of s
+ */
+char *rot13(char *s)
+{
+	int i = 0;
+	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	while (s[i])
+	{
+		j = 0;
+		while (s[i] = a[j])
+		{
+			s[i] = b[j];
+			j++;
+		}
+		i++;
+	}
+}
