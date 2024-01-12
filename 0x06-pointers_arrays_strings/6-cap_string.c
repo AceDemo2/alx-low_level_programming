c61f03c82173c4b0708a1a2590035cde5a459e4f
AceDemo2 <lawal641@gmail.com>
2024-01-12 13:42:55 -0800

    capitalize first letter of a str

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,31 +7,33 @@
  */
 char *cap_string(char *s)
 {
-    char separators[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
-    char *p = s, *p2;
+	char separators[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+	char *p = s, *p2;
 
-    /* Check if the first character is a lowercase letter */
-    if (*s >= 'a' && *s <= 'z')
-        *s -= 32;
+	/* Check if the first character is a lowercase letter */
+	if (*s >= 'a' && *s <= 'z')
+		*s -= 32;
 
-    while (*s)
-    {
-        p2 = separators;
+	while (*s)
+	{
+		p2 = separators;
 
-        /* Iterate through separators */
-        while (*p2)
-        {
-            /* Check for a separator */
-            if (*s == *p2)
-            {
-                /* Check if the next character is a lowercase letter */
-                if (*(s + 1) >= 'a' && *(s + 1) <= 'z')
-                    *(s + 1) -= 32;
-            }
-            p2++;
-        }
+		/* Iterate through separators */
+		while (*p2)
+		{
+			/* Check for a separator */
+			if (*s == *p2)
+			{
+				/* Check if the next character is a lowercase letter */
+				if (*(s + 1) >= 'a' && *(s + 1) <= 'z')
+					*(s + 1) -= 32;
+			}
+			p2++;
+		}
 
-        s++; /* Move to the next character */
-    }
-    return p;
+		s++; /* Move to the next character */
+	}
+
+	return p;
 }
+
