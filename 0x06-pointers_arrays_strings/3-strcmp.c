904b60cfc9ddff3b37b698ca584b9944ba5149cf
AceDemo2 <lawal641@gmail.com>
2024-01-12 07:48:46 -0800

    compare str

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,15 +8,7 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 && *s2)
-	{
-		if (*s1 != *s2)
-			break;
-		else
-		{
-			s1++;
-			s2++;
-		}
-	}
-	return (*s1 - *s2);
+        while (*s1 && *s2)
+		(*s1 != *s2) ? break : s1++, s2++;
+        return (*s1 - *s2);
 }
