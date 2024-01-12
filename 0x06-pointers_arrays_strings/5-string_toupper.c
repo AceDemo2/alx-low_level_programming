fc587c0c510bbf593455b20ecb6d866eead09577
AceDemo2 <lawal641@gmail.com>
2024-01-12 08:16:39 -0800

    lower to upper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,1 +1,18 @@
-sh
+#include "main.h"
+/**
+ * string_toupper - change lowercase to uppercase
+ * @s:string
+ * Return:char
+ */
+char *string_toupper(char *n)
+{
+	char *p = n;
+
+	while (*n)
+	{
+		if (*n >= 'a' and *n <= 'z')
+			*n -= 32;
+		n++;
+	}
+	return (p);
+}
