bfeb07bb431ee5ff51f9b52a963faa995aa91ace
AceDemo2 <lawal641@gmail.com>
2024-01-12 08:07:41 -0800

    rev str

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,1 +1,20 @@
-sh
+#include "main.h"
+/**
+ * reverse_array - reverse array
+ * @a:array
+ * @n:integer
+ * Return:void
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0, temp, j = n - 1;
+
+	while (i < n / 2)
+	{
+		temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+		i++;
+		j--;
+	}
+}
