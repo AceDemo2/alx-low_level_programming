fa6e6360d293a069e9dea780317c92651ca89db8
AceDemo2 <lawal641@gmail.com>
2024-01-12 09:01:55 -0800

    capitalize first letter of a str

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,1 +1,25 @@
-sh
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words in a string
+ * @s: string
+ * Return: address of s
+ */
+char *cap_string(char *s)
+{
+	char n[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'}
+	char *p = s, *p2 = n;
+
+	while (*s)
+	{
+		while (*p2)
+		{
+			if (*s == *p2)
+				*s++ -= 32;
+			p2++;
+		}
+		p2 = n;
+		*s++;
+	}
+	return (p);
+}
