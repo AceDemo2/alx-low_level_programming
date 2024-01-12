43bdca7fdd5f1c7ef3e511ac4e2bde13cd14bfbb
AceDemo2 <lawal641@gmail.com>
2024-01-12 07:33:52 -0800

    copies a string

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,1 +1,23 @@
-sh
+#include "main.h"
+
+/**
+ * _strcmp - compare two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: 0 if strings are equal, negative if s1 < s2, positive if s1 > s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		else
+		{
+			s1++;
+			s2++;
+		}
+	}
+	if (*s1 == *s2)
+		return (0);
+}
