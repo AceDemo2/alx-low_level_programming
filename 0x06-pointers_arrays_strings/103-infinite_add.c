478fb69293234dd51fa44fb11d70193dfa9dcf2a
AceDemo2 <lawal641@gmail.com>
2024-01-13 06:21:01 -0800

    add big numbers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,61 +1,46 @@
 #include "main.h"
-#include <stdint.h>
 #include <stdio.h>
+
 /**
  * infinite_add - adds two numbers
  * @n1: first number
  * @n2: second number
  * @r: buffer for result
  * @size_r: buffer size
- * ahhh! Crazy task! Expand your knowledge
+ *
  * Return: address of r or 0
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i = 0, j = 0, k = 0, l, m = 0, n, temp;
-	char *n3, *n4;
+	int i = 0, j = 0, k = 0, l = 0, m = 0, n = 0;
+	char *n3, *n4, temp;
 
 	while (n1[i])
 		i++;
 	while (n2[j])
 		j++;
-	if (i >= j)
-	{
-		n3 = n1;
-		n4 = n2;
-	}
-	else
-	{
-		n3 = n2;
-		n4 = n1;
-		temp = i;
-        	i = j;
-       		j = temp;
-	}
+	n3 = (i >= j) ? n1 : n2;
+	n4 = (i >= j) ? n2 : n1;
 	i--;
 	j--;
 	while (i >= 0 || j >= 0)
 	{
-		n = ((n3[i]) - '0' + (n4[j]) - '0');
+		n = ((n3[i] - '0') + (n4[j] - '0'));
 		if (n > 9 && i > 0)
-			n3[i - 1] = ((n3[i - 1]) - '0') + 1;
+			n3[i - 1] = ((n3[i - 1] - '0') + 1) + '0';
 		n %= 10;
-		k = k * 10 + n;
+		r[k++] = n + '0';
 		i--;
 		j--;
 	}
-	l = k;
-	while (l> 0)
-	{
-		l /= 10;
-		m++;
-	}
-	if (m > size_r)
+	if (k >= size_r || m > size_r)
 		return (0);
-	else
+	r[k] = '\0';
+	for (i = 0, l = k - 1; i < l; i++, l--)
 	{
-		sprintf(r, "%d", k);
-		return (r);
+		temp = r[i];
+		r[i] = r[l];
+		r[l] = temp;
 	}
+	return (r);
 }
-
