c138b73621e1116ae6f2ee85f47137f3418048fb
AceDemo2 <lawal641@gmail.com>
2024-01-13 05:59:36 -0800

    add big numbers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -12,21 +12,30 @@
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int i = 0, j = 0, k = 0, l, m = 0, n;
+	char *n3, *n4;
 
 	while (n1[i])
 		i++;
 	while (n2[j])
 		j++;
+	if (i >= j)
+	{
+		n3 = n1;
+		n4 = n2
+	}
+	else
+	{
+		n3 = n2;
+		n4 = n1;
+	}
 	i--;
 	j--;
 	while (i >= 0 || j >= 0)
 	{
-		n = ((n1[i]) - '0' + (n2[j]) - '0');
-		if (n > 9)
-		{
-			n1[i - 1] = ((n1[i - 1]) - '0') + 1;
-			n %= 10;
-		}
+		n = ((n3[i]) - '0' + (n4[j]) - '0');
+		if (n > 9 && n1[i - 1])
+			n3[i - 1] = ((n3[i - 1]) - '0') + 1;
+		n %= 10;
 		k = k * 10 + n;
 		i--;
 		j--;
