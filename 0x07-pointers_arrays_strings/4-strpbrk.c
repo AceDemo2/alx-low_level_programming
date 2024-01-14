c2ca080bf4274da7903a686ab343e600cdbb429f
AceDemo2 <lawal641@gmail.com>
2024-01-14 09:21:59 -0800

    locate first char

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,3 +1,30 @@
+#include "main.h"
+
+/**
+ * _strpbrk - Search a string for any of a set of bytes.
+ * @s: string
+ * @accept: string to match
+ * Return: Pointer to the byte in `s` that matches one of the bytes in `accept`
+ * or NULL if no such byte is found.
+ */
 char *_strpbrk(char *s, char *accept)
 {
+	int i = 0, j;
+	char *p;
+
+	while (s[i])
+	{
+		j = 0;
+		while (accept[j])
+		{
+			if (accept[j] == s[i])
+			{
+				p = &s[i];
+				return (p);
+			}
+			j++;
+		}
+		i++;
+	}
+}
 
