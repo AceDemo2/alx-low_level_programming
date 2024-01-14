d7c91fa3d5b2db3556b78fefb2ed5f4f005d26e7
AceDemo2 <lawal641@gmail.com>
2024-01-13 17:03:25 -0800

    count lenght for substring

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strspn - a function that gets the length of a prefix substring
+ *
+ * @s: the initial segment of
+ *
+ * @accept: which consist only of bytes from
+ *
+ * Return: the number of bytes
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i = 0, j, k;
+
+	while (s[i])
+	{
+		j = 0;
+		while (accept[j])
+		{
+			if (accept[j] == s[i])
+				k += 0;
+			j++;
+		}
+		i++;
+	}
+	return (k);
+}
