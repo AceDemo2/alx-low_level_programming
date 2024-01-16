f24f0e731891a4d4d7745a0eb873db184e0ad21c
AceDemo2 <lawal641@gmail.com>
2024-01-15 22:42:32 -0800

    sqrt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,21 @@
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: integer to find sqrt of
+ * Return: natural square root or -1
+ */
+
+int _sqrt_recursion(int n)
+{
+	int i = 0;
+	
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	e = 0.0000000001;
+	while (((i * i) - n) < 0)
+	{
+		i = (i + n / i) * 0.5
+	}
+	return (i);
+}	
