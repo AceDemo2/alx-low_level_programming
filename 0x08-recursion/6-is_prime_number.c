2869d58fdc68a426bca44bbc1f370c94cff4c5ff
AceDemo2 <lawal641@gmail.com>
2024-01-16 04:03:27 -0800

    prime num

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,8 +6,10 @@
  */
 int prime(int a, int b)
 {
-	if (a <= 0 || (a % b == 0))
-	       return (0);
+	if (a <= 1)
+		return (0);
+	if (a % b == 0)
+		return (0);
 	if (b == 1)
 		return (1);
 	return prime(a, b - 1);
