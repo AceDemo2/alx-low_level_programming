4154322989ffb186522b7c89ea66748b40fb6f41
AceDemo2 <lawal641@gmail.com>
2024-01-16 03:52:33 -0800

    prime num

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,21 @@
+/**
+ * prime - checks to see if number is prime
+ * @a:int
+ * @b:int
+ * Return:int
+ */
+int prime(int a, int b)
+{
+	if (a <= 0 || (a % b == 1))
+	       return (0);
+	if (b == 1)
+		return (1);
+	return (a, b - 1);
+}
+/**
+ * is_prime_number - states if number is prime
+ * @n:int
+ * Return:int
+ */
+int is_prime_number(int n)
+	return (n, n - 1);
