efa787585505d7f660a756f338c0b00946c8619f
AceDemo2 <lawal641@gmail.com>
2024-01-17 07:14:21 -0800

    memory allocatio

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+
+/**
+ * *create_array - creates an array of chars,
+ * and initializes it with a specific char
+ * @size: size of the array to create
+ * @c: char to initialize the array c
+ *
+ * Return: pointer to the array (Success), NULL (Error)
+ */
+char *create_array(unsigned int size, char c);
+{
+	char i[];
+
+	if (size == 0)
+		return (0);
+	i = (char *)malloc(sizeof(char) * size);
+	if (i == NULL)
+		return (0);
+	while (j < size - 1)
+	{
+		i[j] = c;
+		j++;
+	}
+	return (i);
+}
