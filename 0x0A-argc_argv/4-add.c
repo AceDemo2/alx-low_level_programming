2ce9ab02476a35654ea6bcd5a231d51fb6290016
AceDemo2 <lawal641@gmail.com>
2024-01-16 17:56:31 -0800

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,44 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+
 /**
- * main - adds positive numbers.
- * @argc: number of command line arguments.
- * @argv: array that contains the program command line arguments.
+ * main - Adds positive numbers.
+ * @argc: Number of command line arguments.
+ * @argv: Array that contains the program command line arguments.
+ *
  * Return: 0 - success.
  */
-
 int main(int argc, char *argv[])
 {
-        int i, sum = 0, j;
+	int i, sum = 0, j;
 
-        if (argc > 1)
-        {
-                i = 1;
-                while (argv[i])
-                {
+	if (argc > 1)
+	{
+		i = 1;
+		while (argv[i])
+		{
 			j = 0;
 			while (argv[i][j])
 			{
-                        	if (argv[i][j] - '0' >= 0 && argv[i][j] - '0' <= 9)
-                        	{
-        	                        j++;
-                	        }
-                 	        else
-                        	{
-                               		printf("Error\n");
-                                	return (1);
-                        	}
-                	}
+				if (argv[i][j] >= '0' && argv[i][j] <= '9')
+				{
+					j++;
+				}
+				else
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
 			sum += atoi(argv[i]);
 			i++;
 		}
-                printf("%d\n", sum);
-                return (0);
-        }
-        else
-        {
-                printf("0\n");
-                return (0);
-        }
+		printf("%d\n", sum);
+		return (0);
+	}
+	else
+	{
+		printf("0\n");
+		return (0);
+	}
 }
+
