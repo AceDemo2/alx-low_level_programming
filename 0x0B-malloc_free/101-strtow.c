51838eb7d504035677abcf64829def51c39a6ed2
AceDemo2 <lawal641@gmail.com>
2024-01-19 14:23:41 -0800

    split str

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -22,23 +22,26 @@ int count_words(char *str) {
 }
 
 char **strtow(char *str) {
-    if (str == NULL || *str == '\0') {
+    char **words;
+	int num_words;
+	 int word_index;
+	 int word_length;
+	 int in_word;
+	 int i;
+	 if (str == NULL || *str == '\0') {
         return NULL;
     }
 
-    int num_words = count_words(str);
+    num_words = count_words(str);
 
-    char **words = (char **)malloc((num_words + 1) * sizeof(char *));
+    words = (char **)malloc((num_words + 1) * sizeof(char *));
     if (words == NULL) {
         return NULL;
     }
 
-    int word_index = 0;
-    int word_length = 0;
-    int in_word = 0;
-
-    int i; 
-
+    word_index = 0;
+    word_length = 0;
+    in_word = 0;
     while (*str) {
         if (is_space(*str)) {
             if (in_word) {
