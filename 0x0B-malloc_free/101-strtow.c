f6a348566d5d54769ee1eba35c6e789a8074a4f9
AceDemo2 <lawal641@gmail.com>
2024-01-20 05:04:32 -0800

    split str

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -8,7 +8,7 @@
  */
 int is_space(char c)
 {
-    return (c == ' ');
+	return (c == ' ');
 }
 
 /**
@@ -19,24 +19,24 @@ int is_space(char c)
  */
 int count_words(char *str)
 {
-    int count = 0;
-    int in_word = 0;
+	int count = 0;
+	int in_word = 0;
 
-    while (*str)
-    {
-        if (is_space(*str))
-        {
-            in_word = 0;
-        }
-        else if (!in_word)
-        {
-            in_word = 1;
-            count++;
-        }
-        str++;
-    }
+	while (*str)
+	{
+		if (is_space(*str))
+		{
+			in_word = 0;
+		}
+		else if (!in_word)
+		{
+			in_word = 1;
+			count++;
+		}
+		str++;
+	}
 
-    return count;
+	return count;
 }
 
 /**
@@ -47,80 +47,80 @@ int count_words(char *str)
  */
 char **strtow(char *str)
 {
-    char **words;
-    int num_words;
-    int word_index;
-    int word_length;
-    int in_word;
-    int i;
+	char **words;
+	int num_words;
+	int word_index;
+	int word_length;
+	int in_word;
+	int i, j;
 
-    num_words = count_words(str);
+	num_words = count_words(str);
 
-    if (str == NULL || num_words == 0)
-        return NULL;
+	if (str == NULL || num_words == 0)
+		return NULL;
 
-    words = (char **)malloc((num_words + 1) * sizeof(char *));
-    if (words == NULL)
-        return NULL;
+	words = (char **)malloc((num_words + 1) * sizeof(char *));
+	if (words == NULL)
+		return NULL;
 
-    word_index = 0;
-    word_length = 0;
-    in_word = 0;
+	word_index = 0;
+	word_length = 0;
+	in_word = 0;
 
-    while (*str)
-    {
-        if (is_space(*str))
-        {
-            if (in_word)
-            {
-                words[word_index] = (char *)malloc((word_length + 1) * sizeof(char));
-                if (words[word_index] == NULL)
-                {
-                    for (i = 0; i < word_index; i++)
-                        free(words[i]);
-                    free(words);
-                    return NULL;
-                }
+	while (*str)
+	{
+		if (is_space(*str))
+		{
+			if (in_word)
+			{
+				words[word_index] = (char *)malloc((word_length + 1) * sizeof(char));
+				if (words[word_index] == NULL)
+				{
+					for (i = 0; i < word_index; i++)
+						free(words[i]);
+					free(words);
+					return NULL;
+				}
 
-                for (i = 0; i < word_length; i++)
-                    words[word_index][i] = *(str - word_length + i);
+				for (i = 0; i < word_length; i++)
+					words[word_index][i] = *(str - word_length + i);
 
-                words[word_index][word_length] = '\0';
+				words[word_index][word_length] = '\0';
 
-                word_index++;
-                word_length = 0;
-                in_word = 0;
-            }
-        }
-        else
-        {
-            word_length++;
-            in_word = 1;
-        }
-        str++;
-    }
+				word_index++;
+				word_length = 0;
+				in_word = 0;
+			}
+		}
+		else
+		{
+			word_length++;
+			in_word = 1;
+		}
+		str++;
+	}
 
-    if (in_word)
-    {
-        words[word_index] = (char *)malloc((word_length + 1) * sizeof(char));
-        if (words[word_index] == NULL)
-        {
-            for (i = 0; i <= word_index; i++)
-                free(words[i]);
-            free(words);
-            return NULL;
-        }
+	if (in_word)
+	{
+		words[word_index] = (char *)malloc((word_length + 1) * sizeof(char));
+		if (words[word_index] == NULL)
+		{
+			for (i = 0; i <= word_index; i++)
+				free(words[i]);
+			free(words);
+			return NULL;
+		}
 
-        for (i = 0; i < word_length; i++)
-            words[word_index][i] = *(str - word_length + i);
+		for (i = 0; i < word_length; i++)
+			words[word_index][i] = *(str - word_length + i);
 
-        words[word_index][word_length] = '\0';
+		words[word_index][word_length] = '\0';
 
-        word_index++;
-    }
+		word_index++;
+	}
 
-    words[word_index] = NULL;
+	words[word_index] = NULL;
 
-    return words;
+	return words;
 }
 
