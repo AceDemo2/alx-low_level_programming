323e9f6df5b0af6db58f737a33e43abb212d7226
AceDemo2 <lawal641@gmail.com>
2024-01-20 02:33:28 -0800

    split str

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -28,7 +28,7 @@ char **strtow(char *str) {
 	 int word_length;
 	 int in_word;
 	 int i;
-        if (str == NULL || (*str == '\0' || (*str == ' ' && *(str + 1) == '\0'))){
+	 if (str == NULL || (*str == '\0' && *(str + 1) == '\0')) {
 	 return NULL;
     }
 
