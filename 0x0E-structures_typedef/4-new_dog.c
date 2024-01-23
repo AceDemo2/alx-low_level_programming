45a5aec36162a31a776bec0fd77597f7e324a245
AceDemo2 <lawal641@gmail.com>
2024-01-23 04:41:37 -0800

    create copy

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - creates a new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Return: pointer to new dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	struct *i;
+
+	i = malloc(sizeof(dog_t));
+	if (i = NULL)
+		return (NULL);
+	i->name = strdup(name);
+	i->owner = strdup(owner);
+	if (i->name == NULL || i->owner == NULL)
+	{
+		free(i->name);
+		free(i->owner);
+		return (NULL);
+	}
+	i->age = age;
+	return (i);
+}
