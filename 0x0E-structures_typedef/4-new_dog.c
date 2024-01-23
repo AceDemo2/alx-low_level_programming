1bce095ab4906c85f8d38a20505ca6e35b167dfe
AceDemo2 <lawal641@gmail.com>
2024-01-23 05:16:59 -0800

    create copy

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -22,12 +22,16 @@ dog_t *new_dog(char *name, float age, char *owner)
 	i->name = strdup(name);
 	if (i->name == NULL)
 	{
-		free(i->name);
+		free(i);
+		return (NULL);
 	}
 	i->owner = strdup(owner);
 	if (i->owner == NULL)
 	{
-		free(i->owner);
+		free(i->name);
+		free(i);
+		return (NULL);
+
 	}
 	i->age = age;
 	return (i);
