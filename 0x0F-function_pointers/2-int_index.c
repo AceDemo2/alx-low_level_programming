e349aa2142b84b9b138bf08748a709abd9a15f2d
AceDemo2 <lawal641@gmail.com>
2024-01-25 09:44:19 -0800

    check array index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include "function_pointers.h"
+
+/**
+  * int_index - ...
+  * @array: ...
+  * @size: ...
+  * @cmp: ...
+  *
+  * Return: ...
+  */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size <= 0 || !array)
+		return (-1);
+	if (array)
+	{
+		while(i < size)
+		{
+			if (cmp(array[i]))
+				break;
+			i+;
+		}
+		return (i);
+	}
+	if (!array[i])
+		return (-1);
+}
