0f515af2d352ab8840035543de2874d9df1440e8
AceDemo2 <lawal641@gmail.com>
2024-01-31 11:29:20 -0800

    calc

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,28 @@
+#include "3-calc.h"
+
+/**
+ * main - Entry point of the program
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line argument strings
+ *
+ * Return: 0 on success, 98, 99, or 100 on failure
+ */
+int main(int argc, char *argv[])
+{
+	int r;
+	int (*i)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+        	exit(98);
+	}
+	i = get_op_func(argv[2]);
+	if (i == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	printf("%d\n", i(atoi(argv[1]), atoi(argv[3]))); 
+	return (0);
+}
