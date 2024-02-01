94d5b59d1729ddc2b3f5be98201352e1e03ab530
AceDemo2 <lawal641@gmail.com>
2024-01-31 21:29:31 -0800

    print num

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -22,7 +22,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 			printf("%d%s", va_arg(i, int), separator);
 			j++;
 		}
-	}
 	printf("%d\n", va_arg(i, int));
+	}
 	va_end(i);
 }
