1ac401923c6455aee2f1a93c1c6c6a18f79bf698
AceDemo2 <lawal641@gmail.com>
2024-02-02 14:56:40 -0800

    print anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -6,42 +6,42 @@
  */
 void print_all(const char *const format, ...)
 {
-    const char *k = format;
-    char *j;
-    va_list i;
+	const char *k = format;
+	char *j;
+	va_list i;
 
-    va_start(i, format);
+	va_start(i, format);
 
-    while (k && *k)
-    {
-        switch (*k)
-        {
-        case 'c':
-            printf("%c", va_arg(i, int));
-            break;
-        case 'i':
-            printf("%d", va_arg(i, int));
-            break;
-        case 'f':
-            printf("%f", va_arg(i, double));
-            break;
-        case 's':
-            j = va_arg(i, char *);
-            if (!j)
-                j = "(nil)";
-            printf("%s", j);
-            break;
-        default:
-            break;
-        }
+	while (k && *k)
+	{
+		switch (*k)
+		{
+		case 'c':
+			printf("%c", va_arg(i, int));
+			break;
+		case 'i':
+			printf("%d", va_arg(i, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(i, double));
+			break;
+		case 's':
+			j = va_arg(i, char *);
+			if (!j)
+				j = "(nil)";
+			printf("%s", j);
+			break;
+		default:
+			break;
+		}
 
-        if ((*k == 'c' || *k == 'i' || *k == 'f' || *k == 's') && *(k + 1))
-            printf(", ");
+		if ((*k == 'c' || *k == 'i' || *k == 'f' || *k == 's') && *(k + 1))
+			printf(", ");
 
-        k++;
-    }
+		k++;
+	}
 
-    va_end(i);
-    printf("\n");
+	va_end(i);
+	printf("\n");
 }
 
