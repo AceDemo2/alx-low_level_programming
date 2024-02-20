e2dc65bff37aeed31aea1b9b15397ff0918c1a57
AceDemo2 <lawaladepoju705@gmail.com>
2024-02-20 05:07:48 -0800

    binary to deci

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,13 +8,13 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int i = 0;
+	unsigned int i = 0;
 
 	if (b == NULL)
 		return (0);
 	while (*b)
 	{
-		if (*b != 0 || *b != 1)
+		if (*b != '0' || *b != '1')
 			return (0);
 		i = (i << 1) | (*b - '0');
 		b++;
