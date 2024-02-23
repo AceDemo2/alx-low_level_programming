d31afa5ea9bee200226870d714eae4c72e9c26f4
AceDemo2 <lawaladepoju705@gmail.com>
2024-02-23 15:40:20 -0800

    print safe

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,40 +1,36 @@
 #include "lists.h"
-
 /**
- * print_listint_safe - prints a linked list, safely
- * @head: list of type listint_t to print
- *
- * Return: number of nodes in the list
+ * print_listint_safe - function that prints a linked list with a loop safely.
+ * @head: pointer to the 1st node of the linked list
+ * Return: new_node
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *slow = head, *fast = head;
+	const listint_t *tmp_n = NULL;
+	const listint_t *l_n = NULL;
 	size_t counter = 0;
+	size_t new_n;
 
-	while (fast != NULL && fast->next != NULL)
+	tmp_n = head;
+	while (tmp_n)
 	{
-		printf("[%p] %d\n", (void *)slow, slow->n);
+		printf("[%p] %d\n", (void *)tmp_n, tmp_n->n);
 		counter++;
-		slow = slow->next;
-		fast = fast->next->next;
-
-		if (slow == fast)
+		tmp_n = tmp_n->next;
+		l_n = head;
+		new_n = 0;
+		while (new_n < counter)
 		{
-			printf("-> [%p] %d\n", (void *)slow, slow->n);
-
-			slow = head;
-			while (slow != fast)
+			if (tmp_n == l_n)
 			{
-				printf("[%p] %d\n", (void *)slow, slow->n);
-				counter++;
-				slow = slow->next;
-				fast = fast->next;
+				printf("-> [%p] %d\n", (void *)tmp_n, tmp_n->n);
+				return (counter);
 			}
-
-			return counter;
+			l_n = l_n->next;
+			new_n++;
 		}
+		if (!head)
+			exit(98);
 	}
-
-	return counter;
+	return (counter);
 }
-
