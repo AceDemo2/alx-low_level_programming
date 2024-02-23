455a2312a8368ec1c273beb6b53488c3aa957d58
AceDemo2 <lawaladepoju705@gmail.com>
2024-02-23 04:07:08 -0800

    print safe

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,32 +8,33 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-    const listint_t *slow = head, *fast = head;
-    size_t counter = 0;
+	const listint_t *slow = head, *fast = head;
+	size_t counter = 0;
 
-    while (fast != NULL && fast->next != NULL)
-    {
-        printf("[%p] %d\n", (void *)slow, slow->n);
-        counter++;
-        slow = slow->next;
-        fast = fast->next->next;
+	while (fast != NULL && fast->next != NULL)
+	{
+		printf("[%p] %d\n", (void *)slow, slow->n);
+		counter++;
+		slow = slow->next;
+		fast = fast->next->next;
 
-        if (slow == fast)
-        {
-            printf("-> [%p] %d\n", (void *)slow, slow->n);
+		if (slow == fast)
+		{
+			printf("-> [%p] %d\n", (void *)slow, slow->n);
 
-            slow = head;
-            while (slow != fast)
-            {
-                printf("[%p] %d\n", (void *)slow, slow->n);
-                counter++;
-                slow = slow->next;
-                fast = fast->next;
-            }
+			slow = head;
+			while (slow != fast)
+			{
+				printf("[%p] %d\n", (void *)slow, slow->n);
+				counter++;
+				slow = slow->next;
+				fast = fast->next;
+			}
 
-            return counter;
-        }
-    }
+			return counter;
+		}
+	}
 
-    return counter;
+	return counter;
 }
+
