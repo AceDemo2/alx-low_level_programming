ebbfe3b69d1f64d2650cfedf57f2d4892052cfdb
AceDemo2 <lawaladepoju705@gmail.com>
2024-02-22 20:19:31 -0800

    insert node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,51 +1,51 @@
 #include "lists.h"
-
 /**
  * insert_nodeint_at_index - adds a new node to a linked list
- * at a given position
- * @head: pointer to the head of the list
- * @idx: index of the list where the new node will be added (indices start at 0)
- * @n: value for the new node to be added
- * Return: pointer to the new node, or NULL if function fails
+ *                           at a given position
+ * @head : pointer to the head of the list
+ * @idx  : index of the list where the new node will be added
+ *         (indices start at 0)
+ * @n    : value for the new node to be added
+ * Return: pointer to the new node,
+ *         or NULL - if function fails
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-    listint_t *new_node, *temp;
-    unsigned int i;
+	listint_t *i = *head, *k = *head;
+	unsigned int j = 0;
 
-    if (head == NULL)
-        return NULL;
-
-    new_node = malloc(sizeof(listint_t));
-    if (new_node == NULL)
-        return NULL;
-
-    new_node->n = n;
-    new_node->next = NULL;
-
-    if (idx == 0)
-    {
-        new_node->next = *head;
-        *head = new_node;
-        return new_node;
-    }
-
-    temp = *head;
-
-    for (i = 0; temp && i < idx - 1; i++)
-    {
-        temp = temp->next;
-    }
-
-    if (temp == NULL)
-    {
-        free(new_node);
-        return NULL;
-    }
-
-    new_node->next = temp->next;
-    temp->next = new_node;
-
-    return new_node;
+	if (head == NULL)
+		return (NULL);
+	while (i)
+	{
+		i = i->next;
+		j++;
+	}
+	if (idx > j)
+		return (NULL);
+	i = malloc(sizeof(listint_t));
+	if (i == NULL)
+		return (NULL);
+	i->n = n;
+	j = 0;
+	if (idx == 0)
+	{
+		i->next = *head;
+		*head = i;
+		return (i);
+	}
+	while (k && j < idx - 1)
+	{
+		k = k->next;
+		j++;
+	}
+	if (k == NULL)
+	{
+		free(i);
+		return (NULL);
+	}
+	i->next = k->next;
+	k->next = i;
+	return (i);
 }
 
