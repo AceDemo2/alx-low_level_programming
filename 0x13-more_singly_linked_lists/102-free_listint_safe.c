566f6416ff413807a825e6580c64726f7e4c3303
AceDemo2 <lawaladepoju705@gmail.com>
2024-02-24 07:00:30 -0800

    find loop

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -7,20 +7,45 @@
  */
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *i;
-	size_t k = 0;
+    listint_t *slow = *h, *fast = *h;
+    size_t nodes_freed = 0;
 
-	if (h == NULL || *h == NULL)
-		return (k);
-	while (*h)
-	{
-		i = (*h)->next;
-		free(*h);
-		*h = i;
-		k++;
-		if (!i)
-			break;
-	}
-	*h = NULL;
-	return (k);
+    /* Detect the loop using Floyd's algorithm */
+    while (fast && fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast)
+            break;
+    }
+
+    if (!fast || !fast->next)
+    {
+        /* No loop, simply free the list */
+        while (*h)
+        {
+            listint_t *temp = *h;
+            *h = temp->next;
+            free(temp);
+            nodes_freed++;
+        }
+    }
+    else
+    {
+        /* Loop detected, free the looped part */
+        slow = *h;
+        while (slow != fast)
+        {
+            listint_t *temp_slow = slow;
+            slow = slow->next;
+            free(temp_slow);
+            nodes_freed++;
+        }
+    }
+
+    *h = NULL; /* Set the head to NULL after freeing */
+
+    return nodes_freed;
 }
+
