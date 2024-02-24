56129fc87967d502e471a2828b2ef2986ebd3ac5
AceDemo2 <lawaladepoju705@gmail.com>
2024-02-23 18:21:45 -0800

    free list with loop

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -8,7 +8,7 @@
 size_t free_listint_safe(listint_t **h)
 {
 	listint_t *i;
-	size_t k = 0, l = sizeof(listint_t);
+	size_t k = 0, l = sizeof(listint_t), m;
 
 	if (h == NULL || *h == NULL)
 		return (k);
@@ -18,7 +18,9 @@ size_t free_listint_safe(listint_t **h)
 		free(*h);
 		*h = i;
 		k++;
-		(sizeof(i) == l) ? continue : break; 
+		m = sizeof(i);
+		if (l != m)
+			break;	
 	}
 	return (k);
 }
