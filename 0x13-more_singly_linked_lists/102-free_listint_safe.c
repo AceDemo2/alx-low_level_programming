0524205850ee6b9ea31a510bd92ebaf3bcae2607
AceDemo2 <lawaladepoju705@gmail.com>
2024-02-24 15:44:54 -0800

    free list

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,4 +1,6 @@
 #include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * free_listint_safe - frees a listint_t list
@@ -7,36 +9,62 @@
  */
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *i = *h, *j;
-	size_t k = 0, l;
+    listint_t *slow, *fast, *temp;
+    size_t count = 0;
 
-	if (h == NULL || *h == NULL)
-		return (k);
-	while (*h)
-	{
-		i = i->next;
-		k++;
-		j = *h;
-		l = 0;
-		while (l < k)
-		{
-			if (i == j)
-				break;
-			j = j->next;
-			l++;
-		}
-		if (i == j)
-			break;
-	}
-	l = 0;
-	*h = i;
-	while (l < k)
-	{
-		i = (*h)->next;
-		free(*h);
-		*h = i;
-		l++;
-	}
-	*h = NULL;
-	return (k);
+    if (h == NULL || *h == NULL)
+        return (count);
+
+    slow = *h;
+    fast = *h;
+
+    while (slow && fast && fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast)
+        {
+            /* Loop detected, break the loop */
+            slow = *h;
+            while (slow != fast)
+            {
+                temp = fast;
+                fast = fast->next;
+                count++;
+                free(temp);
+            }
+
+            /* Set the last node in the loop to NULL */
+            temp->next = NULL;
+
+            /* Continue freeing the rest of the list */
+            while (*h)
+            {
+                temp = (*h)->next;
+                free(*h);
+                *h = temp;
+                count++;
+            }
+
+            /* Set the head to NULL to indicate the list is freed */
+            *h = NULL;
+            return (count);
+        }
+    }
+
+    /* No loop, free the entire list */
+    while (*h)
+    {
+        temp = (*h)->next;
+        free(*h);
+        *h = temp;
+        count++;
+    }
+
+    /* Set the head to NULL to indicate the list is freed */
+    *h = NULL;
+
+    return (count);
 }
+
