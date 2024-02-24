3effa4c093588e59bd18e2229c6036f48eb82667
AceDemo2 <lawaladepoju705@gmail.com>
2024-02-23 18:08:25 -0800

    free list with loop

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,40 +1,25 @@
 #include "lists.h"
 
 /**
- * free_listint_safe - frees a linked list
- * @h: pointer to the first node in the linked list
- *
- * Return: number of elements in the freed list
+ * free_listint_safe - frees a listint_t list
+ * @h: pointer to the head of the list
+ * Return: the size of the list that was freed
  */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t len = 0;
-	int diff;
-	listint_t *temp;
+	listint_t *i;
+	size_t k = 0;
 
-	if (!h || !*h)
-		return (0);
-
+	if (h == NULL || *h == NULL)
+		return (k);
 	while (*h)
 	{
-		diff = *h - (*h)->next;
-		if (diff > 0)
-		{
-			temp = (*h)->next;
-			free(*h);
-			*h = temp;
-			len++;
-		}
-		else
-		{
-			free(*h);
-			*h = NULL;
-			len++;
+		i = ((*h)->next != NULL) ? (*h)->next : NULL;
+		free(*h);
+		*h = i;
+		k++;
+		if (i == NULL)
 			break;
-		}
 	}
-
-	*h = NULL;
-
-	return (len);
+	return (k);
 }
