476b062a466a2cf80a82451b4ebcac53ed5aa843
AceDemo2 <lawaladepoju705@gmail.com>
2024-02-25 20:38:21 -0800

    read file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -29,7 +29,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		free(j);
 		return (0);
 	}
-	w = write(1, j, letters);
+	w = write(1, j, r);
 	if (w == -1 || w != r)
 	{
 		fclose(i);
