ddad3cb39319230f7ef54e77182e53f81dbf1cf7
AceDemo2 <lawaladepoju705@gmail.com>
2024-02-26 06:03:06 -0800

    copy file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,26 +11,27 @@ void print_error_and_exit(int exit_code, const char *message, const char *file_n
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 3) {
+    char *file_from, *file_to, buffer[BUFFER_SIZE];
+    int fd_from, fd_to;
+    ssize_t bytes_read, bytes_written;
+	if (argc != 3) {
         print_error_and_exit(97, "Usage: cp file_from file_to\n", NULL);
     }
 
-    char *file_from = argv[1];
-    char *file_to = argv[2];
+    file_from = argv[1];
+    file_to = argv[2];
 
-    int fd_from = open(file_from, O_RDONLY);
+    fd_from = open(file_from, O_RDONLY);
     if (fd_from == -1) {
         print_error_and_exit(98, "Error: Can't read from file %s\n", file_from);
     }
 
-    int fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+    fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
     if (fd_to == -1) {
         close(fd_from);
         print_error_and_exit(99, "Error: Can't write to file %s\n", file_to);
     }
 
-    char buffer[BUFFER_SIZE];
-    ssize_t bytes_read, bytes_written;
 
     while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0) {
         bytes_written = write(fd_to, buffer, bytes_read);
