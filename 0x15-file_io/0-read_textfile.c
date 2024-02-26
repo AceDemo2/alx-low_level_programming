465ff04a9c5ae1fe834142033a21a1e7ac1a9ba5
AceDemo2 <lawaladepoju705@gmail.com>
2024-02-25 19:08:28 -0800

    read file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * read_textfile - reads a text file and prints it to the standard output
+ * @filename: name of the file to be read
+ * @letters: number of letters to read and print
+ * Return: the number of letters printed, or 0 if it failed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *i;
+	char *j;
+	ssize_t r, w;
+
+	if (filename == NULL)
+		return (0);
+	i = fopen(filename, "r");
+	if (i == NULL)
+		return (0);
+	j = malloc(letters);
+	if (j == NULL)
+	{
+		fclose(i);
+		return (0);
+	}
+	r = read(j, 1, letters), i;
+	fclose(i);
+	if (r == -1)
+	{
+		free(j);
+		return (0);
+	}
+	w = write(1, j, letters);
+	if (w == -1)
+		return (0);
+	free(j);
+	return (letters);
+}
