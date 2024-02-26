15c016441ff09ad2cf3bfd713a0d6ef6c26b24f0
AceDemo2 <lawaladepoju705@gmail.com>
2024-02-26 05:14:49 -0800

    write to file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,30 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content writed in the file.
+ *
+ * Return: 1 if it success. -1 if it fails.
+ */
 int create_file(const char *filename, char *text_content)
 {
 	int i;
 
-	i = open(filename, 
+	if (filname == NULL)
+		return (-1);
+	i = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (i == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		r = write(i, text_content, strlen(text_content));
+		if (r == -1)
+		{
+			close(i);
+			return (-1);
+		}
+	}
+	close(i);
+	return (1);
+}
