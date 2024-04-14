15b8e6ba6e2814f4ae0a5d71dcb2ded9b370c54f
AceDemo2 <lawaladepoju705@gmail.com>
2024-04-14 08:43:20 -0700

    add at the end

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -18,7 +18,10 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	j->n = n;
 	j->next = NULL;
 	if (*head == NULL)
+	{
 		j->prev = *head;
+		*head = j;
+	}
 	else
 	{
 		while (i->next)
