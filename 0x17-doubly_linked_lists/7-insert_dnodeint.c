a20db46fd56f2f2ce4d1f927152efb11d07bebb0
AceDemo2 <lawaladepoju705@gmail.com>
2024-04-14 17:13:12 -0700

    new node

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,7 +13,7 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *i = *h, *j;
-	
+
 	if (!idx)
 		return (add_dnodeint(h, n));
 	j = malloc(sizeof(dlistint_t));
